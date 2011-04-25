7602a2fa94daeef6222ff104fbffe991a8917769
Luis Sala <lsala@primogen.com>
2011-04-25 06:28:04 -0700

    Second part of prior commit.

--- apps/movies/resources/main_page.js
+++ apps/movies/resources/main_page.js
@@ -95,7 +95,74 @@ Movies.previewImageView = SC.View.extend({
     touchEnd: function(evt) {
         // Tell FlowPlayer to play the video.
         // "layerId" is the ID SproutCore assigned this view/anchor tag.
-        //$f(this.get("layerId")).play();
+        $f(this.get("a.video-player")).play(this.get("videoUrl"));
         return YES;
     }
 });
+
+Movies.VideoPlayer = SC.View.extend({
+
+    uri: null,
+
+    tagName: "a",
+
+    classNames: ["video-player"],
+
+    // This view expects a video URL and preview image URL.
+    displayProperties: ["videoUrl", "previewUrl"],
+
+    // Once the view is created, we want to tell FlowPlayer
+    // to bind itself to this element/layer.
+    didCreateLayer: function(){
+        var id = this.get("layerId");
+
+        // Invoke the FlowPlayer function and enable auto-play along with
+        // using the HTML5 video tag for non-iOS devices.
+        this.invokeLater(function(){
+                $f(id, "http://sala.us/flowplayer/flowplayer-3.2.7.swf", {
+                    clip:  {
+                        autoPlay: true,
+                        autoBuffering: true
+                    }
+                }).ipad({simulateiDevice: false});
+            }, 0);
+    }, // end didCreateLayer()
+
+
+    // Flowplayer requires an HTML anchor <a href="..."> tag pointing to a
+    // video, styled with the preview image (splash) and overlayed with the
+    // "play" button.
+    render: function(context) {
+		sc_super();
+
+        // Add href attribute.
+        context.attr("href", this.get("videoUrl"));
+
+        // Add preview/splash image.
+        context.attr("style", "background-image:url("+this.get("previewUrl")+")");
+
+        // Insert <IMG> tag with play button image.
+        context.begin("img").attr("src", sc_static("resources/images/play_large")).end();
+    }, // end render()
+
+    update: function(context) {
+		sc_super();
+        this.$().attr("href", this.get("videoUrl"));
+    }, // end update()
+
+    captureTouch: function(){
+		return YES;
+	},
+
+    touchStart: function(evt) {
+        return YES;
+    },
+
+    // If this player element is touched, then play the corresponding video.
+    touchEnd: function(evt) {
+        // Tell FlowPlayer to play the video.
+        // "layerId" is the ID SproutCore assigned this view/anchor tag.
+        $f(this.get("a.video-player")).play();
+        return YES;
+    }
+}); // end VideoPlayer
